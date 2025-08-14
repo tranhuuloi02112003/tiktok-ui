375cf08a20fe24de128d85be3e73d6476987a4d3
tranhuuloi02112003 <tranhuuloi2k3@gmail.com>
2025-08-14 18:06:36 +0700

    chore: configure routing and project settings

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Fragment } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { publicRoutes } from "@/routes";
+import { DefaultLayout } from "@/components/Layout";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Router>
+      <Routes>
+        {publicRoutes.map((route, index) => {
+          let Layout = DefaultLayout;
+
+          if(route.layout) {
+            Layout = route.layout
+          }else if(route.layout === null) {
+            Layout = Fragment; 
+          }
+
+          return (
+            <Route key={index} path={route.path} element={<Layout>{route.element}</Layout>} />
+          );
+        })}
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
